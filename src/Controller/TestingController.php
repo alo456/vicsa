c6c1b4af4747fd942b44ebeaac6ea8da33008cda
Victor Cv <victorcv017@gmail.com>
2018-10-17 20:42:45 -0500

    Routes yaml

--- src/Controller/TestingController.php
+++ src/Controller/TestingController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class TestingController extends AbstractController
 {
-    /**
-     * @Route("/testing", name="testing")
-     */
     public function index()
     {
-        return $this->render('testing/index.html.twig');
+        return $this->render('home.html.twig');
     }
+    
+    public function test()
+    {
+        return $this->render('login.html.twig');
+    }
+    
+    
 }
